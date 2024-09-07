4a1867a85882580268c9c941ed8e5df9c9f5a733
Irregular Bismuth <elliot.s@live.se>
2024-09-07 23:24:40 +0200

    work in progress with contact

--- src/contact.rs
+++ src/contact.rs
@@ -1,11 +1,21 @@
-struct Contact {
+use crate::kademlia_id::KademliaID;
+pub struct Contact {
+    id: KademliaID,
     address: String,
+    pub distance: usize,
 }
 
 impl Contact {
-    pub fn new() -> Self {
+    pub fn new(id: KademliaID, address: String) -> Self {
         Self {
+            id,
             address: "".to_string(),
+            distance: 0,
         }
+    }
+
+    pub fn calc_distance(&mut self, target: &KademliaID) -> &mut Self {
+        self.distance = target.distance(&self.id);
+        self
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use kadrustlia::cli::Cli;
 
+use kadrustlia::contact::Contact;
 use kadrustlia::kademlia_id::KademliaID;
 async fn run() {
     println!("Test");
@@ -14,11 +15,12 @@ async fn main() {
     let kad_id: KademliaID = KademliaID::new();
 
     let mut kad_id = KademliaID::new();
-    let kad_id2 = KademliaID::new().store_data("test".to_string()).to_hex();
-
+    let kad_id2 = KademliaID::new();
+    let mut contact = Contact::new(kad_id, "2123".to_string());
+    contact.calc_distance(&kad_id2);
+    println!("Contact distance to target: {}", contact.distance);
     println!("{}", kad_id.to_hex());
     println!("{}", kad_id.store_data("test".to_string()).to_hex());
-    println!("{}", kad_id2);
     let cli = Cli::new();
     cli.read_input().await;
 }

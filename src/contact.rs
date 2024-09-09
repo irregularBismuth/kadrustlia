3a75639c5c4854ca4510489ecb3991e79a4b7c50
irregularBismuth <elliot.s@live.se>
2024-09-09 09:05:25 +0200

    started with contact candidates

--- src/contact.rs
+++ src/contact.rs
@@ -19,3 +19,20 @@ impl Contact {
         self
     }
 }
+
+pub struct ContactCandidates {
+    contacts: Vec<Contact>,
+}
+
+impl ContactCandidates {
+    pub fn append(&mut self, contacts: &mut Vec<Contact>) -> &mut Self {
+        self.contacts.append(contacts);
+        self
+    }
+
+    pub fn get_contacts(&mut self, count: usize) -> &mut [Contact] {
+        let len = self.contacts.len();
+        let end = count.min(len);
+        &mut self.contacts[0..end]
+    }
+}

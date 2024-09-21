34b1e1c3e32a03e6f3f225e23836308ad1a11cdd
Irregular Bismuth <elliot.s@live.se>
2024-09-21 18:42:10 +0200

    start on routing table handler

---
+++ src/routing_table_handler.rs
@@ -0,0 +1,25 @@
+pub enum RouteTableCMD {
+    AddContact(Contact),
+    RemoveContact(KademliaID),
+    GetClosestNodes(KademliaID),
+}
+
+pub async fn routing_table_handler(
+    mut rx: mpsc::Receiver<RouteTableCMD>,
+    mut routing_table: RoutingTable,
+) {
+    println!("route table handler");
+    while let Some(cmd) = rx.recv().await {
+        match cmd {
+            RouteTableCMD::AddContact(contact) => {
+                println!("ping  hello ");
+            }
+            RouteTableCMD::RemoveContact(kad_id) => {
+                println!("remove  coibntact");
+            }
+            RouteTableCMD::GetClosestNodes(kad_id) => {
+                println!("kademlia we got {}", kad_id.to_hex());
+            }
+        }
+    }
+}
